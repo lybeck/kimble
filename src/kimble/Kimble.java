b93dc35d4d74d50f87c827512e3a36166b52dab1
Lasse Lybeck <lybeck.lasse@gmail.com>
2014-05-08 16:09:22 +0300

    Added constant for automatic turns

--- src/kimble/Kimble.java
+++ src/kimble/Kimble.java
@@ -21,33 +21,34 @@ import org.lwjgl.util.vector.Vector3f;
  * @author Lasse Lybeck
  */
 public class Kimble {
-    
+
     private Game game;
-    
+
     private BoardGraphic board;
     private List<PieceGraphic> pieces;
-    
+
     private Camera camera;
     private Shader shader;
-    
+
+    private static final boolean AUTOMATIC_TURNS = true;
     private static final float TURN_TIME_STEP = 0.1f;
     private static final boolean DEBUG = false;
-    
+
     public Kimble() {
-        
+
         setup();
-        
+
         while (!Screen.isCloseRequested()) {
             Screen.clear();
-            
+
             float dt = 0.016f;
-            
+
             update(dt);
             render();
-            
+
             Screen.update(60);
         }
-        
+
         shader.cleanUp();
         board.cleanUp();
         for (PieceGraphic p : pieces) {
@@ -55,28 +56,28 @@ public class Kimble {
         }
         Screen.cleanUp();
     }
-    
+
     private void setup() {
         camera = new Camera(new Vector3f(20, 70, -20), new Vector3f((float) (Math.PI / 3.0), 0, 0), 70f, 0.3f, 1000f);
-        
+
         int numberOfTeams = 4;
         int numberOfPieces = 4;
         int sideLength = 8;
-        
+
         this.game = new Game(Constants.DEFAULT_START_VALUES, Constants.DEFAULT_CONTINUE_TURN_VALUES, numberOfTeams, numberOfPieces, sideLength);
-        
+
         board = new BoardGraphic(game, 10, 3, 1);
         shader = new Shader("res/shaders/shader.vert", "res/shaders/shader.frag");
-        
+
         camera.setPosition(new Vector3f(0, board.getRadius() * 1.5f, board.getRadius() * 1.2f));
-        
+
         pieces = new ArrayList<>();
         for (int i = 0; i < game.getTeams().size(); i++) {
             for (Piece p : game.getTeam(i).getPieces()) {
                 pieces.add(new PieceGraphic(board, p, new Vector3f(0, 0, 0), BoardGraphic.teamColors.get(i), 4, 10));
             }
         }
-        
+
         GameStart gameStart = game.startGame();
         System.out.println("Game start:");
         System.out.println(gameStart.getRolls());
@@ -89,44 +90,46 @@ public class Kimble {
 
 //        Mouse.setGrabbed(true);
     }
-    
+
     private float timer = 0;
     private float cameraPositionAngle = 0;
-    
+
     private void update(float dt) {
-        
+
         cameraPositionAngle += dt * 0.1;
         camera.setPosition(new Vector3f(board.getRadius() * 1.2f * (float) Math.cos(cameraPositionAngle), board.getRadius() * 1.5f, board.getRadius() * 1.2f * (float) Math.sin(cameraPositionAngle)));
         camera.setRotation(new Vector3f((float) (Math.PI / 3.0), cameraPositionAngle - (float) Math.PI / 2, 0));
-        
+
         if (Screen.wasResized()) {
             Screen.updateViewport();
             camera.updateProjectionMatrixAttributes();
         }
-        
+
         while (Keyboard.next()) {
             if (Keyboard.isKeyDown(Keyboard.KEY_RETURN)) {
                 executeMove();
             }
         }
-        timer += dt;
-        if (timer >= TURN_TIME_STEP) {
-            executeMove();
-            timer = 0;
+        if (AUTOMATIC_TURNS) {
+            timer += dt;
+            if (timer >= TURN_TIME_STEP) {
+                executeMove();
+                timer = 0;
+            }
         }
-        
+
         camera.update(dt);
         board.update(dt);
-        
+
         for (PieceGraphic p : pieces) {
             p.update(dt);
         }
     }
-    
+
     Random random = new Random();
-    
+
     int lastKey = -1;
-    
+
     private void executeMove() {
         Turn nextTurn = game.getNextTurn();
         if (DEBUG) {
@@ -148,9 +151,9 @@ public class Kimble {
             System.out.println("");
         }
     }
-    
+
     private void render() {
-        
+
         shader.bind();
         board.render(shader);
         for (PieceGraphic p : pieces) {
@@ -169,7 +172,7 @@ public class Kimble {
         Screen.setupDisplay("Kimble - alpha 0.1", 800, 600);
         Screen.setupOpenGL();
         Screen.setResizable(true);
-        
+
         new Kimble();
     }
 
