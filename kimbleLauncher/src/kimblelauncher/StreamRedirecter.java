c5eb3bf526e0aa787e5683a3f00a99d1004f0601
Christoffer <christoffer.fridlund@hotmail.com>
2014-06-06 16:02:10 +0300

    Renamed the KimbleLauncher to kimbleLauncher

---
+++ kimbleLauncher/src/kimblelauncher/StreamRedirecter.java
@@ -0,0 +1,37 @@
+package kimblelauncher;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Christoffer
+ */
+public class StreamRedirecter implements Runnable {
+
+    private final BufferedReader bufferedReader;
+    private final PrintStream destination;
+
+    public StreamRedirecter(InputStream inputStream, PrintStream destination) throws UnsupportedEncodingException {
+        this.bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"));
+        this.destination = destination;
+    }
+
+    @Override
+    public void run() {
+        String line;
+        try {
+            while ((line = bufferedReader.readLine()) != null) {
+                destination.println(line);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(StreamRedirecter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
